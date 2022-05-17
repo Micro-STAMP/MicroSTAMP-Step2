5196c33b84829eb9d2d95a0d6248823469b43bf2
João Hugo <joaohmarinho99@gmail.com>
2022-05-16 21:35:33 -0300

    Liberando /variables e /states para teste

--- src/main/java/Step2FormTest/configuration/WebSecurityConfig.java
+++ src/main/java/Step2FormTest/configuration/WebSecurityConfig.java
@@ -78,7 +78,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         "/guests/**")*/
             web.ignoring()
                     .antMatchers("/guests/**","/static/**","/webjars/**","/assets/**", "/guests-request/**")
-                    .antMatchers("/controlstructures/**", "/components/**", "/actuators/**", "/sensors/**", "/controllers/**","/controlledProcesses/**","/connections/**","/images/**","/labels/**")
+                    .antMatchers("/controlstructures/**", "/components/**", "/actuators/**", "/sensors/**", "/controllers/**","/controlledProcesses/**","/connections/**","/images/**","/labels/**", "/variables/**", "/states/**")
                     .antMatchers(HttpMethod.OPTIONS, "/**");
     }
 }
